99c00d9a7a590e35be69f6d4a6c356c1c1f3de6d
jiangmenglin <jiangmenglin@aliyun.com>
2019-09-21 11:03:14 +0800

    内存管理

---
+++ memry.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    char *description;
+    strcpy(name, "Zara Ali");
+
+    description = (char*) malloc(30 * sizeof(char));
+    if (description == NULL)
+    {
+        fprintf(stderr, "Error - unable to allocate required memry\n");
+    }
+    else
+    {
+        strcpy(description, "Zara Ali a DSP student");
+    }
+
+    description = (char *)  realloc(description, 100 * sizeof(char));
+    if (description == NULL)
+    {
+        fprintf(stderr, "Error - unable to allocate reqiured memery.\n");
+    }
+    else
+    {
+        strcat(description, "She is in class 10th.");
+    }
+
+    printf("Name = %s\n", name);
+    printf("Description: %s\n,", description);
+    free(description);
+}
